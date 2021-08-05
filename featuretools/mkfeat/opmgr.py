93dde99fa548710b3751e11c9500178ea3e69722
JungHyun <leahincom@gmail.com>
2021-08-05 20:31:46 +0900

    Set OperatorManager to handle all primitives according to the type

--- featuretools/mkfeat/opmgr.py
+++ featuretools/mkfeat/opmgr.py
@@ -1,18 +1,17 @@
+import featuretools as ft
+from featuretools.primitives import utils
+
 class OperatorManager:
     """
-    현재 설계상으로는 특징 추출시 연산자를 사용자가 지정하도록 되어 있으나, 사용자가 연산자를 지정하지 않는 방식으로 특징 추출이 구현될
-    수 있음. 이러한 경우를 지원하는 용도로 이 클래스가 향후 활용될 수 있음
-    지금 구현에서는 사용자는 단순히 operator 이름만 지정하는데, featuretool의 dfs 입력은 transform이나 aggregation 타입을 구분하여
-    operator를 지정해야 함. 이를 위해 사용자가 지정한 operator에 대해 해당 유형을 적절히 반환할 수 있도록 해야 함
+    현재 설계상으로는 특징 추출시 연산자를 사용자가 지정하도록 되어 있으나, 사용자가 연산자를 지정하지 않는 방식으로 특징 추출이 구현될 수 있음.
+    이러한 경우를 지원하는 용도로 이 클래스가 향후 활용될 수 있음
+    지금 구현에서는 사용자는 단순히 operator 이름만 지정하는데, featuretool의 dfs 입력은 transform이나 aggregation 타입을 구분하여 operator를 지정해야 함.
+    이를 위해 사용자가 지정한 operator에 대해 해당 유형을 적절히 반환할 수 있도록 해야 함
     """
     def __init__(self, operators):
-        self.operators = set(operators)
-        self.transforms = {
-            "absolute", "day", "haversine", "month", "num_characters", "num_words", "weekday", "year"
-        }
-        self.aggregations = {
-            "count", "max", "mode", "num_unique", "percent_true", "skew",  "std", "sum"
-        }
+        self.operators = set(str.lower(op) for op in operators)
+        self.transforms = utils.get_transform_primitives().keys()
+        self.aggregations = utils.get_aggregation_primitives().keys()
 
     def get_transform_operators(self):
         return set(self.operators & self.transforms)
