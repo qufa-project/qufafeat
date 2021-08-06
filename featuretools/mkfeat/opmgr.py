47634ea04a4eb28f087f04b02ebdf68323e7c1c4
JungHyun <leahincom@gmail.com>
2021-08-06 17:02:39 +0900

    OperatorManager: set all operators as default

--- featuretools/mkfeat/opmgr.py
+++ featuretools/mkfeat/opmgr.py
@@ -3,15 +3,24 @@ from featuretools.primitives import utils
 
 class OperatorManager:
     """
-    현재 설계상으로는 특징 추출시 연산자를 사용자가 지정하도록 되어 있으나, 사용자가 연산자를 지정하지 않는 방식으로 특징 추출이 구현될 수 있음.
-    이러한 경우를 지원하는 용도로 이 클래스가 향후 활용될 수 있음
-    지금 구현에서는 사용자는 단순히 operator 이름만 지정하는데, featuretool의 dfs 입력은 transform이나 aggregation 타입을 구분하여 operator를 지정해야 함.
-    이를 위해 사용자가 지정한 operator에 대해 해당 유형을 적절히 반환할 수 있도록 해야 함
+    1.
+    현재 설계 상으로는 특징 추출시 연산자를 사용자가 지정하도록 되어 있으나, 사용자가 연산자를 지정하지 않는 방식으로 특징 추출이 구현될 수 있음.
+    이러한 경우, 전체 operator를 반환하여 특징 추출을 지원하는 용도로 이 클래스를 활용함.
+    2.
+    지금 구현에서는 사용자가 단순히 operator 이름만 지정하는데, featuretool의 dfs 입력은 transform이나 aggregation 타입을 구분하여 operator를 지정해야 함.
+    이를 위해 사용자가 지정한 operator에 해당하는 유형을 반환함.
     """
     def __init__(self, operators):
-        self.operators = set(str.lower(op) for op in operators)
+        if not operators:
+            self.operators = self.get_all_operators()
+        else:
+            self.operators = set(str.lower(op) for op in operators)
         self.transforms = utils.get_transform_primitives().keys()
         self.aggregations = utils.get_aggregation_primitives().keys()
+
+    def get_all_operators(self):
+        operators_df = ft.list_primitives()
+        return set(operators_df['name'])
 
     def get_transform_operators(self):
         return set(self.operators & self.transforms)
