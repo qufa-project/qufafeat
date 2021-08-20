f74cfd420cae51e151678962678504de1b68d5ac
KyungWoon Cho <cezanne@codemayo.com>
2021-08-21 05:44:42 +0900

    Apply more reasonable feature selection
    MinMaxScaler를 통하여 0과 1사이로 값을 정규화한 경우, variance를
    0.02정도로 맞추는 것이 적당해 보임.
    featuretool의 유사 컬럼 제거를 적용

--- featuretools/mkfeat/featsel.py
+++ featuretools/mkfeat/featsel.py
@@ -3,13 +3,15 @@ from sklearn.preprocessing import MinMaxScaler
 
 from pandas import DataFrame
 
+from featuretools import selection
 
-THRESHOLD = 0.2
+THRESHOLD = 0.02
 
 
 def select_features(df: DataFrame, features):
     df_new = DataFrame()
     features_new = []
+    f_names_check = []
 
     for feat in features:
         name = feat.get_name()
@@ -21,6 +23,7 @@ def select_features(df: DataFrame, features):
                 scaler.fit(arr)
                 sel.fit(scaler.transform(arr))
                 if sel.variances_[0] >= THRESHOLD:
+                    f_names_check.append(name)
                     df_new[name] = df[name]
                     features_new.append(feat)
             except ValueError:
@@ -29,4 +32,6 @@ def select_features(df: DataFrame, features):
             df_new[name] = df[name]
             features_new.append(feat)
 
+    df_new, features_new = selection.remove_highly_correlated_features(df_new, features_new,
+                                                                       features_to_check=f_names_check)
     return df_new, features_new
